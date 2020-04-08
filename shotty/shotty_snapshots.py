8171552a6b5ea3b55192378e5cd456ed039427ce
Sundar Shanmuganathan <sundarshan2018@gmail.com>
2020-04-08 12:00:12 -0400

    Added stop/start/waitime of instances when creating snapshot

--- shotty/shotty_snapshots.py
+++ shotty/shotty_snapshots.py
@@ -83,9 +83,19 @@ def create_snapshot(project):
     instances =filter_instances(project)
 
     for i in instances:
+        print ("Stopping instance {0}".format(i.id))
+        i.stop()
+        i.wait_until_stopped()
+
         for v in i.volumes.all():
             print("Creating Snapshot of volume {0}".format(v.id))
             v.create_snapshot(Description="Snapshot created by snapshotanalyzer 30000")
+
+        print ("Starting instance {0}".format(i.id))
+        i.start()
+        i.wait_until_running()
+    print ("Job done")
+
     return
 
 
