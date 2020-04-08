9ddcd6c6451edac416f665b80f90268061ebb497
Sundar Shanmuganathan <sundarshan2018@gmail.com>
2020-04-07 20:32:07 -0400

    Filter Instances by project. Also stop,start, list instances

--- shotty/shotty.py
+++ shotty/shotty.py
@@ -6,15 +6,26 @@ session=boto3.Session(profile_name='shotty')
 ec2=session.resource('ec2')
 
 @click.command()
-def list_instances():
+@click.option('--project', default=None , help="Only Instances of project (tag Project:<name>)")
+def list_instances(project):
     "List All EC2 Instances"
-    for i in ec2.instances.all():
+    instances=[]
+
+    if project:
+        filters=[{'Name': 'tag:Project', 'Values': [project]}]
+        instances = ec2.instances.filter(Filters=filters)
+    else:
+        instances = ec2.instances.all()
+
+    for i in instances:
+        tags = { t['Key'] : t['Value'] for t in i.tags or []}
         print(', '.join((
          i.id,
          i.instance_type,
          i.placement['AvailabilityZone'],
          i.state['Name'],
-         i.public_dns_name)))
+         i.public_dns_name,
+         tags.get('Project','<no project>'))))
     return
 
 
